002e118967f0f7e0b22fbc66c3f193c2b76c6a1a
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-11 15:55:18 -0300

    add data inspection

--- chapter_2/input_data.py
+++ chapter_2/input_data.py
@@ -1,8 +1,9 @@
 import os
 import tarfile
-from six.moves import urllib
 
+import matplotlib.pyplot as plt
 import pandas as pd
+from six.moves import urllib
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -22,3 +23,20 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
+
+
+def inspect_data(df):
+    # Useful DataFrame descriptors
+    print("<<< INFO >>>>\n")
+    df.info()
+    print(f"\n\n"
+          f"<<< VALUE COUNTS >>>\n"
+          f"{df['ocean_proximity'].value_counts()}\n\n"
+          f"<<< DESCRIPTION >>>\n"
+          f"{df.describe()}\n\n")
+
+    # Draw a histogram for each numerical attribute
+    fig, ax = plt.subplots(figsize=(12, 8))
+    df.hist(bins=50, ax=ax)
+    os.makedirs('chapter_2/resources', exist_ok=True)
+    fig.savefig('chapter_2/resources/housing_data.png')
