9db692c4c58c5cb405ee38647c14375150171552
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-12 17:50:41 -0300

    add test set separation

--- chapter_2/main.py
+++ chapter_2/main.py
@@ -1,7 +1,8 @@
-from chapter_2 import input_data
+from chapter_2 import input_data, test_set
 
 if __name__ == "__main__":
     input_data.fetch_housing_data()
     housing = input_data.load_housing_data()
     input_data.inspect_data(housing)
+    train_test = test_set.split_train_test(housing, 0.2)
 
