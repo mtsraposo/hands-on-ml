703f2eb6960cba89392e130cce55147ff475778e
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-13 13:18:54 -0300

    add config for urls and paths

--- src/hands_on_ml/chapter_2/input_data.py
+++ src/hands_on_ml/chapter_2/input_data.py
@@ -5,23 +5,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from six.moves import urllib
 
-DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
-HOUSING_PATH = os.path.join("datasets", "housing")
-HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
-
-def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
-    if not os.path.isdir(housing_path):
-        os.makedirs(housing_path)
-    tgz_path = os.path.join(housing_path, "housing.tgz")
+def fetch_housing_data(config_data):
+    if not os.path.isdir(config_data['housing_path']):
+        os.makedirs(config_data['housing_path'])
+    tgz_path = os.path.join(config_data['housing_path'], "housing.tgz")
+    housing_url = os.path.join(config_data['download_root'], config_data['download_path'])
     urllib.request.urlretrieve(housing_url, tgz_path)
     housing_tgz = tarfile.open(tgz_path)
-    housing_tgz.extractall(path=housing_path)
+    housing_tgz.extractall(path=config_data['housing_path'])
     housing_tgz.close()
 
 
-def load_housing_data(housing_path=HOUSING_PATH):
-    csv_path = os.path.join(housing_path, "housing.csv")
+def load_housing_data(config_data):
+    csv_path = os.path.join(config_data['housing_path'], "housing.csv")
     return pd.read_csv(csv_path)
 
 
@@ -38,5 +35,9 @@ def inspect_data(df):
     # Draw a histogram for each numerical attribute
     fig, ax = plt.subplots(figsize=(12, 8))
     df.hist(bins=50, ax=ax)
-    os.makedirs('chapter_2/resources', exist_ok=True)
-    fig.savefig('chapter_2/resources/housing_data.png')
+    os.makedirs('src/hands_on_ml/chapter_2/resources', exist_ok=True)
+    fig.savefig('src/hands_on_ml/chapter_2/resources/housing_data.png')
+
+
+def inspect_training_set(df):
+    plt.show()
