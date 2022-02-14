07eb4a01218eab885f772526be940203ca0eacca
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-14 15:06:50 -0300

    add treatment to categorical attributes

--- src/hands_on_ml/chapter_2/data_cleaning.py
+++ src/hands_on_ml/chapter_2/data_cleaning.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sklearn.impute import SimpleImputer
 
+
 def fill_with_median(housing):
     imputer = SimpleImputer(strategy='median')
-    housing_num = housing.drop('ocean_proximity', axis=1) # drop non-numerical column
+    housing_num = housing.drop('ocean_proximity', axis=1)  # drop non-numerical column
     imputer.fit(housing_num)
     X = imputer.transform(housing_num)
-    return pd.DataFrame(X, columns=housing_num.columns)
-
+    housing_tr = pd.DataFrame(X, columns=housing_num.columns)
+    return housing_tr
--- src/hands_on_ml/chapter_2/feature_engineering.py
+++ src/hands_on_ml/chapter_2/feature_engineering.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 
 
 def create_categories(data):
@@ -16,7 +17,27 @@ def drop_categories(train_test):
 
 
 def append_ratios(data):
-    data['rooms_per_household'] = data['total_rooms']/data['households']
-    data['bedrooms_per_room'] = data['total_bedrooms']/data['total_rooms']
-    data['population_per_household'] = data['population']/data['households']
+    data['rooms_per_household'] = data['total_rooms'] / data['households']
+    data['bedrooms_per_room'] = data['total_bedrooms'] / data['total_rooms']
+    data['population_per_household'] = data['population'] / data['households']
     return data
+
+
+def gen_ordinal_encoding(housing):
+    """
+    Assigns a numerical value to each category on a given column
+    """
+    ordinal_encoder = OrdinalEncoder()
+    housing_cat = housing[['ocean_proximity']]
+    housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+    return housing_cat_encoded
+
+
+def gen_onehot_encoding(housing):
+    """
+    Assigns 1 to 'hot' categories and 0 to other categories on a given column
+    """
+    cat_encoder = OneHotEncoder()
+    housing_cat = housing[['ocean_proximity']]
+    housing_cat_1hot = cat_encoder.fit_transform(housing_cat)  # returns a SciPy sparse matrix
+    return housing_cat_1hot
